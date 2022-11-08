5f1aee40fcceb6013aa92f0ddaeff27e3b4b311c
delphine.richard <delphine.richard@alten.com>
2022-11-08 17:54:39 +0100

    wip: update homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatButtonModule } from "@angular/material/button";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCard, MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    ToolbarComponent,
-    DashboardComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomepageComponent,
+		ToolbarComponent,
+		DashboardComponent,
+	],
+	imports: [
+		AppRoutingModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatCardModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
