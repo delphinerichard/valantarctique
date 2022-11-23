c919043b339b21607fcddffccf3aa7b979cb9188
delphine.richard <delphine.richard@alten.com>
2022-11-23 17:02:13 +0100

    wip: add firebase databse

--- src/app/admin/post-list/post-list.component.ts
+++ src/app/admin/post-list/post-list.component.ts
@@ -1,39 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+import { app } from "src/app/app.module";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	Firestore,
+} from "firebase/firestore/lite";
 
 export interface Post {
-  title: string;
-  updated: Date;
-  content: string;
+	id: number;
+	title: string;
+	updated: Date;
+	content: string;
 }
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+	selector: "app-post-list",
+	templateUrl: "./post-list.component.html",
+	styleUrls: ["./post-list.component.scss"],
 })
 export class PostListComponent implements OnInit {
+	postList: Post[] = [];
 
-  postList: Post[] = [
-    {
-      title: 'Photos',
-      updated: new Date('1/1/16'),
-      content: 'bonjour'
-    },
-    {
-      title: 'Recipes',
-      updated: new Date('1/17/16'),
-      content: 'bonjour'
-    },
-    {
-      title: 'Work',
-      updated: new Date('1/28/16'),
-      content: 'bonjour'
-    },
-  ];
+	constructor(private _snackBar: MatSnackBar) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		const db = getFirestore(app);
+		this.getPosts(db).then((list: Post[]) => {
+			this.postList = list;
+		});
+	}
 
-  ngOnInit(): void {
-  }
+	// Get a list of posts from your database
+	async getPosts(db: Firestore): Promise<Post[]> {
+		const postsCol = collection(db, "posts");
+		const postSnapshot = await getDocs(postsCol);
+		let postList = postSnapshot.docs.map((doc) => doc.data());
+		postList = postList.map((post) => {
+			return { ...post, updated: new Date(post["updated"].seconds * 1000) };
+		});
+		return postList as Post[];
+	}
 
+	onDelete(id: number): void {
+		this._snackBar.open(`Suppression de l'article ${id}`, "OK", {
+			duration: 5000,
+		});
+	}
+
+	onUpdate(id: number): void {
+		this._snackBar.open(`Modification de l'article ${id}`, "OK", {
+			duration: 5000,
+		});
+	}
+
+	onDisplay(id: number): void {
+		this._snackBar.open(`Affichage de l'article ${id}`, "OK", {
+			duration: 5000,
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,33 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
-import {MatListModule} from '@angular/material/list'; 
+import { MatListModule } from "@angular/material/list";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 import { AuthModule } from "@auth0/auth0-angular";
-import { PostListComponent } from './admin/post-list/post-list.component';
+import { PostListComponent } from "./admin/post-list/post-list.component";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAtD0gKHcqFaqd0it7pZQDQfwwFm4sWSdE",
+	authDomain: "valantarctique-b7206.firebaseapp.com",
+	projectId: "valantarctique-b7206",
+	storageBucket: "valantarctique-b7206.appspot.com",
+	messagingSenderId: "395652256191",
+	appId: "1:395652256191:web:1c56ea2ba30c265d55910a",
+	measurementId: "G-1KTGHT8SXS",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
 	declarations: [
@@ -25,7 +48,7 @@ import { PostListComponent } from './admin/post-list/post-list.component';
 		ToolbarComponent,
 		DashboardComponent,
 		AuthButtonComponent,
-  PostListComponent,
+		PostListComponent,
 	],
 	imports: [
 		AppRoutingModule,
@@ -36,6 +59,7 @@ import { PostListComponent } from './admin/post-list/post-list.component';
 		MatIconModule,
 		MatCardModule,
 		MatListModule,
+		MatSnackBarModule,
 		AuthModule.forRoot({
 			domain: "dev-gfb1q8bbhz7br08r.us.auth0.com",
 			clientId: "sIHSEXYJmLc2PGLRbqxLr0Vme7SOvWEG",
